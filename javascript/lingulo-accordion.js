e69541e0012d49fc94a7b495c5362c4fdf4e941e
Ivan Aedler <aef23@yandex.com>
2017-05-23 19:18:43 -0300

    Add files via upload

---
+++ javascript/lingulo-accordion.js
@@ -0,0 +1,7 @@
+$(function()
+{var closedHeight=35;var speedClose=150;var speedOpen=300;var acc="#lingulo-accordion";var elem="h";var old=null;$(acc+" > div "+ elem+":first-child").click(function()
+{var i=$(this).parent().index();if(old!==i)
+{$(acc+" > div:eq("+ old+")").animate({height:closedHeight+"px"},speedClose);var curHeight=$(acc+" > div:eq("+ i+")").height();var autoHeight=$(acc+" > div:eq("+ i+")").css('height','auto').height();$(acc+" > div:eq("+ i+")").height(curHeight).animate({height:autoHeight},speedOpen,function()
+{$(acc+" > div:eq("+ i+")").css('height','auto');});$(acc+" > div:eq("+ i+")").animate({height:"auto"});old=i;}
+else
+{$(acc+" > div:eq("+ old+")").animate({height:closedHeight+"px"});old=null;}});});
