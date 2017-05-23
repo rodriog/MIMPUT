2d8ff9248b2379a094fff97d52d232be02f820d0
Ivan Aedler <aef23@yandex.com>
2017-05-23 20:00:08 -0300

    Add files via upload

--- javascript/lingulo-accordion.js
+++ javascript/lingulo-accordion.js
@@ -1,7 +1,11 @@
 $(function()
-{var closedHeight=35;var speedClose=150;var speedOpen=300;var acc=".lingulo-accordion";var elem=$("h3","h4");var old=null;$(acc+" > div "+ elem+":first-child").click(function()
-{var i=$(this).parent().index();if(old!==i)
-{$(acc+" > div:eq("+ old+")").animate({height:closedHeight+"px"},speedClose);var curHeight=$(acc+" > div:eq("+ i+")").height();var autoHeight=$(acc+" > div:eq("+ i+")").css('height','auto').height();$(acc+" > div:eq("+ i+")").height(curHeight).animate({height:autoHeight},speedOpen,function()
-{$(acc+" > div:eq("+ i+")").css('height','auto');});$(acc+" > div:eq("+ i+")").animate({height:"auto"});old=i;}
-else
-{$(acc+" > div:eq("+ old+")").animate({height:closedHeight+"px"});old=null;}});});
+{var closedHeight=35;var speedClose=150;var speedOpen=300;var acc=".lingulo-accordion"; var tags=["h3","h4"];var old=null;
+for(var i = 0; i < tags.length; i++) {
+	$(acc+" > div "+ elem+":first-child").click(function(){
+		var i=$(this).parent().index();if(old!==i)
+		{$(acc+" > div:eq("+ old+")").animate({height:closedHeight+"px"},speedClose);var curHeight=$(acc+" > div:eq("+ i+")").height();var autoHeight=$(acc+" > div:eq("+ i+")").css('height','auto').height();$(acc+" > div:eq("+ i+")").height(curHeight).animate({height:autoHeight},speedOpen,function()
+		{$(acc+" > div:eq("+ i+")").css('height','auto');});$(acc+" > div:eq("+ i+")").animate({height:"auto"});old=i;}
+		else
+		{$(acc+" > div:eq("+ old+")").animate({height:closedHeight+"px"});old=null;}});
+}
+});
