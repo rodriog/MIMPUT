47be5fe529c69ef265003300b2a6a9644d2f3d60
Ivan Aedler <aef23@yandex.com>
2017-05-23 22:15:15 -0300

    Add files via upload

--- javascript/w3data.js
+++ javascript/w3data.js
@@ -5,8 +5,9 @@ function lingulo() {
 	//lingulo-accordion
 	$('.lingulo-accordion > div').on('click', function () {
 		//alert ("OK");
-		alert ($(this).className);
-		if ($(this.className)=="clicked") { //if its the current div clicked - CLOSE IT ONLY.
+		//alert ($(this).className); //DOESNT WORK with jquery
+		//alert ($(obj).attr('class'));
+		if ($(obj).attr('class')==="clicked") { //if its the current div clicked - CLOSE IT ONLY.
 			$('.lingulo-accordion > div').removeClass('clicked'); //close any former clicked class
 		}else {
 			//if ($(".clicked")[0]) { // CLOSE ANY, but ADD a current one, as its an DIFFERENT one that is clicked!
