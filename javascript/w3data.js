92959cffaf69c3323d944096040ec6f0ea466e91
Ivan Aedler <aef23@yandex.com>
2017-05-23 22:55:03 -0300

    Add files via upload

--- javascript/w3data.js
+++ javascript/w3data.js
@@ -5,31 +5,29 @@ function lingulo() {
 	//lingulo-accordion
 	$('.lingulo-accordion > div h3').on('click', function () {
 		//alert ("OK");
-		//alert ($(this).className); //DOESNT WORK with jquery
-		//alert ($(this).attr('class'));
-		if ($(this).attr('class')==="clicked") { //if its the current div clicked - CLOSE IT ONLY.
-			$('.lingulo-accordion > div h3').removeClass('clicked'); //close any former clicked class
+		//alert ($(parent).className); //DOESNT WORK with jquery
+		//alert ($(parent).attr('class'));
+		if ($(parent).attr('class')==="clicked") { //if its the current div clicked - CLOSE IT ONLY.
+			$('.lingulo-accordion > div').removeClass('clicked'); //close any former clicked class
 		}else {
 			//if ($(".clicked")[0]) { // CLOSE ANY, but ADD a current one, as its an DIFFERENT one that is clicked!
 			//alert ("clicked exists");
-			$('.lingulo-accordion > div h4').removeClass('clicked'); //close any former clicked class
-			$('.lingulo-accordion > div h3').removeClass('clicked'); //close any former clicked class
-			$(this).addClass('clicked');
+			$('.lingulo-accordion > div').removeClass('clicked'); //close any former clicked class
+			$(parent).addClass('clicked');
 		}
 	});
-	
+
 	$('.lingulo-accordion > div h4').on('click', function () {
 		//alert ("OK");
-		//alert ($(this).className); //DOESNT WORK with jquery
-		//alert ($(this).attr('class'));
-		if ($(this).attr('class')==="clicked") { //if its the current div clicked - CLOSE IT ONLY.
-			$('.lingulo-accordion > div h4').removeClass('clicked'); //close any former clicked class
+		//alert ($(parent).className); //DOESNT WORK with jquery
+		//alert ($(parent).attr('class'));
+		if ($(parent).attr('class')==="clicked") { //if its the current div clicked - CLOSE IT ONLY.
+			$('.lingulo-accordion > div').removeClass('clicked'); //close any former clicked class
 		}else {
 			//if ($(".clicked")[0]) { // CLOSE ANY, but ADD a current one, as its an DIFFERENT one that is clicked!
 			//alert ("clicked exists");
-			$('.lingulo-accordion > div h4').removeClass('clicked'); //close any former clicked class
-			$('.lingulo-accordion > div h3').removeClass('clicked'); //close any former clicked class
-			$(this).addClass('clicked');
+			$('.lingulo-accordion > div').removeClass('clicked'); //close any former clicked class
+			$(parent).addClass('clicked');
 		}
 		
 		/*
